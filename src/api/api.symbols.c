639c38a28e98851c95a892c204cf57ff925cc5e1
The Holy Ghost <holyghost@ip-172-31-85-48.ec2.internal>
2020-07-28 15:32:54 +0000

    API for processing and dispatching simple symbols, in the works

--- src/api/api.symbols.c
+++ src/api/api.symbols.c
@@ -1,5 +1,7 @@
 /* Copyright (C) The Holy Ghost 2020. See the LICENSE file for details */
 
+#include <stdlib.> /* includes malloc and NULL */
+
 #include "variablesymbol.h"
 #include "lambdasymbol.h"
 
@@ -7,6 +9,43 @@
 
 # define _Variable VariablePtr
 # define _Lambda LambdaPtr
+
+/* symbol constructors, destructors */
+
+void *make_lambda_symbol(void *xvalue)
+{
+	LambdaPtr lp = (LambdaPtr)malloc(sizeof LambdaPtr);
+	lp->blob = xvalue;
+	lp->x = NULL; /* raw unprocessed symbol */
+	return (void *)lp;
+}
+
+void free_lambda_symbol(void *lsym)
+{
+	LambdaPtr lp = (LambdaPtr)lsym;
+	if (lp->blob != NULL)
+		free(lp->blob);
+	/* FIXME generic methods or free of _XSymbol in lsym */
+	free(lp);
+}	
+
+void *make_variable_symbol(void *xvalue)
+{
+	Variable vp = (Variable)malloc(sizeof Variable);
+	vp->blob = xvalue;
+	vp->typeid = NULL; /* raw unprocessed symbol */
+	return (void *)vp;
+}
+	
+void free_variable_symbol(void *vsym)
+{
+	VariablePtr vp = (VariablePtr)vsym;
+	if (vp->blob != NULL)
+		free(vp->blob);
+	/* FIXME generic methods or free of _XSymbol in vsym */
+	free(vp);
+}
+
 
 /* symbol accessors*/
 
@@ -16,14 +55,29 @@ void *get_lambda_symbol_value(void *lsym)
 	return lp->blob;
 }
 
-int get_variable_symbol_type(void *vsym);
+int get_variable_symbol_type(void *vsym)
 {
 	VariablePtr vp = (VariablePtr)lsym;
 	return vp->typeid;
 }
 
-void *get_variable_symbol_value(void *vsym);
+void *get_variable_symbol_value(void *vsym)
 {
 	VariablePtr vp = (VariablePtr)lsym;
 	return vp->blob;
+}
+
+/* processor builds a variable with raw data */
+void process_symbol(void *preraw)
+{
+	/* FIXME */
+}
+
+/* This dispatches the variable to the system, after processing.
+ * The symbol enters an environment and can be manipulated further on.
+ */
+
+void dispatch_symbol(void *new_symbol)
+{
+	/* FIXME */
 }
--- src/api/api.symbols.h
+++ src/api/api.symbols.h
@@ -1,9 +1,20 @@
 /* Copyright (C) The Holy Ghost 2020. See the LICENSE file for details */
 
+/* constructors, destructors */
+
+/* FIXME private or public API */
+
+void *make_lambda_symbol(void *);
+void free_lambda_symbol(void *lsym);
+void *make_variable_symbol(void *);
+void free_variable_symbol(void *);
+
 /* Simplest symbol accessors */
 void *get_lambda_symbol_value(void *);
 
 int get_variable_symbol_type(void *);
 void *get_variable_symbol_value(void *);
 
-
+/* dispatch and processing of symbols */
+void process_symbol(void *);
+void dispatch_symbol(void *);
