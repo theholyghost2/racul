2ac0f538202bdf72d663dfcbf7281fd26972f4fc
The Holy Ghost <holyghost@ip-172-31-85-48.ec2.internal>
2020-11-24 12:19:41 +0000

    added start of hawking boltzmann random hash function

---
+++ src/util/hawkingtemperature.c
@@ -0,0 +1,30 @@
+/* Copyright (C) The Holy Ghost 2020. See the LICENSE file for details */
+#include "hawkingtemperature.h"
+
+#include <math.h>
+
+double hawking_temperature(double blackholemass)
+{	
+	return (6.582119569 * pow(10, -16) * pow(299792458, 3)) /
+		(8 * M_PI * 6.6743015 * pow(10, -11) * 1.380649 * pow(10, -23) 
+		 * solarmass);
+}
+
+double hawking_temperature_div_solarmass(double quotient)
+{
+	return (6.582119569 * pow(10, -16) * pow(299792458, 3)) /
+		(8 * M_PI * 6.6743015 * pow(10, -11) * 1.380649 * pow(10, -23) 
+		 * 1.98847 * pow(10,30) / quotient);
+
+}
+
+double hawking_temperature_simple(double solarmass)
+{
+	return (1.2 * pow(10,23) / solarmass);
+}
+
+double hawking_temperature_simple_div_solarmass(double quotient) 
+{
+	return (1.2 * pow(10,23) / (1.98847 * pow(10,30) / quotient));
+
+}
---
+++ src/util/hawkingtemperature.h
@@ -0,0 +1,6 @@
+/* Copyright (C) The Holy Ghost 2020. See the LICENSE file for details */
+
+double hawking_temperature(double); 
+double hawking_temperature_div_solarmass(double); // relative solar mass 
+double hawking_temperature_simple(double); 
+double hawking_temperature_simple_div_solarmass(double); //relative solar mass
