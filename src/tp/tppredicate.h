3ffa0d61931b1f3cb0605ec5b59f9706c70df148
The Holy Ghost <holyghost@ip-172-31-85-48.ec2.internal>
2020-11-24 08:45:51 +0000

    added start of validities

--- src/tp/tppredicate.h
+++ src/tp/tppredicate.h
@@ -5,12 +5,20 @@
 #define OPERATOR_OR "or"
 #define OPERATOR_NOT "not"
 
-#define OPERATOR_LEFT_PARENS "parens-left"
-#define OPERATOR_RIGHT_PARENS "parens-right"
+#define OPERATOR_LEFT_PARENS "("
+#define OPERATOR_RIGHT_PARENS ")"
 
-#define OPERATOR_FUNCTION_CALL "func"
+/* symbol between anterior and posterior */
+#define STRIDE "->" 
+
+#define OPERATOR_FUNCTION_CALL "funcall"
 
 /* Theorem Prover predicate */
+
+/**************************************************************************
+ * NOTE : the theorem prover uses prefix instead of infix or suffix
+ * See validities.h as an example
+ **************************************************************************/
 
 typedef struct _tppredicate {
 	void *antitems; // anterior items
---
+++ src/tp/validities.h
@@ -0,0 +1,6 @@
+/* Copyright (C) The Holy Ghost 2020. See the LICENSE file for details */
+
+const char* validities[] = {
+	"not ( and x y ) -> not x and not y",
+ 	"\0",
+}	
