374e774e43874726841dea5f71912f5df763ec35
The Holy Ghost <holyghost@ip-172-31-85-48.ec2.internal>
2020-11-24 10:08:52 +0000

    added comments to mini stack

--- src/util/ministack.c
+++ src/util/ministack.c
@@ -16,12 +16,13 @@ void free_stack(_StackPtr sp)
 
 void push_on_stack(const char *str, _StackPtr sp)
 {
+	/* maybe memset */
 	sp->stk[++(sp->index)] = strdup(str);
 }
 
 _StackItemPtr pop_from_stack(const char *str, _StackPtr sp)
 {
 	/* does not test for empty stack */
-	return (sp->stk[sp->index--] && sp->stk[index+1] = (const char *)0);
+	return (sp->stk[sp->index--]); /* && sp->stk[index--] = (const char *)0); */
 }
 
